950fe15bd9d2faa07bb3f29e21e098f3b7227cdb
Bartu Çakır <bartuuu_lol@hotmail.com>
2024-09-27 00:31:58 +0300

    f

--- src/components/navbar/Logout.tsx
+++ src/components/navbar/Logout.tsx
@@ -22,12 +22,12 @@ export default function Logout() {
         {loading ? (
           <div className="flex space-x-2 justify-center items-center dark:invert">
             <span className="sr-only">Loading...</span>
-            <div className="h-1 w-1 bg-white rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-            <div className="h-1 w-1 bg-white rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-            <div className="h-1 w-1 bg-white rounded-full animate-bounce"></div>
+            <div className="h-2 w-2 bg-white rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+            <div className="h-2 w-2 bg-white rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+            <div className="h-2 w-2 bg-white rounded-full animate-bounce"></div>
           </div>
         ) : (
-          "Çıkış yap"
+          "Çıkış Yap"
         )}
       </Button>
     </div>
