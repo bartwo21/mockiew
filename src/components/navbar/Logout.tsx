a49df784987ad5e52e4cc3605f4fdcc52be94fb8
Bartu Çakır <bartuuu_lol@hotmail.com>
2024-09-27 00:35:28 +0300

    fix: logout btn

--- src/components/navbar/Logout.tsx
+++ src/components/navbar/Logout.tsx
@@ -18,13 +18,14 @@ export default function Logout() {
       <Button
         className="mr-4 text-gray-400 flex justify-center items-center"
         variant="outline"
+        disabled={loading}
       >
         {loading ? (
-          <div className="flex space-x-2 justify-center items-center dark:invert">
+          <div className="flex space-x-2 justify-center items-center dark:invert mr-[11px]">
             <span className="sr-only">Loading...</span>
-            <div className="h-2 w-2 bg-white rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-            <div className="h-2 w-2 bg-white rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-            <div className="h-2 w-2 bg-white rounded-full animate-bounce"></div>
+            <div className="h-2 w-2 bg-white bg-opacity-60 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+            <div className="h-2 w-2 bg-white bg-opacity-60 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+            <div className="h-2 w-2 bg-white bg-opacity-60 rounded-full animate-bounce"></div>
           </div>
         ) : (
           "Çıkış Yap"
