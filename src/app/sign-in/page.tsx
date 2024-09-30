5f79f4f4a40cf8e76eed423261b332f47a4a3966
Bartu Çakır <bartuuu_lol@hotmail.com>
2024-09-30 14:33:38 +0300

    fix: login ui

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -16,6 +16,7 @@ import { loginWithCreds } from "../../../actions/actions";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import ErrorMessage from "@/components/ui/ErrorMessage";
+import Image from "next/image";
 
 export default function Page() {
   const cookieStore = cookies();
@@ -31,60 +32,70 @@ export default function Page() {
     <div className="flex items-center justify-center md:mt-24 mt-5 px-5">
       <div className="absolute inset-0 bg-grid-pattern -mt-16"></div>
       <div className="absolute inset-0 bg-radial-gradient"></div>
-      <Card className="w-[450px] relative">
-        <CardHeader>
-          <CardTitle>Giriş Yap</CardTitle>
-          <CardDescription>
-            Hesabınıza giriş yaparak devam edin.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form action={loginWithCreds}>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Email</Label>
-                <Input
-                  id="email"
-                  placeholder="Email"
-                  name="email"
-                  type="email"
-                  required
-                />
+      <div className="z-30 flex md:flex-row flex-col gap-20 justify-center items-center bg-zinc-950 p-5 md:p-28 rounded-lg border border-zinc-800">
+        <Image
+          src="/login.png"
+          width={350}
+          height={350}
+          alt="login"
+          unoptimized
+          className="w-[200px] h-[200px] md:w-[350px] md:h-[350px] object-cover rounded-lg"
+        />
+        <Card className="bg-transparent relative border-zinc-900 shadow-lg lg:w-[400px] w-full">
+          <CardHeader>
+            <CardTitle>Giriş Yap</CardTitle>
+            <CardDescription>
+              Hesabınıza giriş yaparak devam edin.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form action={loginWithCreds}>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="name">Email</Label>
+                  <Input
+                    id="email"
+                    placeholder="Email"
+                    name="email"
+                    type="email"
+                    required
+                  />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="password">Şifre</Label>
+                  <Input
+                    id="password"
+                    placeholder="Şifre"
+                    type="password"
+                    name="password"
+                    required
+                  />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <AuthButton title="Giriş Yap" />
+                </div>
+                {errorMessage && (
+                  <ErrorMessage
+                    key={errorTimestamp || new Date().getTime()}
+                    message={errorMessage}
+                  />
+                )}
+                <p className="text-xs opacity-70">
+                  Hesabın yok mu?{" "}
+                  <Link href="/sign-up">
+                    <Button variant="link" className="px-0 text-xs">
+                      Kayıt ol
+                    </Button>
+                  </Link>
+                </p>
               </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="password">Şifre</Label>
-                <Input
-                  id="password"
-                  placeholder="Şifre"
-                  type="password"
-                  name="password"
-                  required
-                />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <AuthButton title="Giriş Yap" />
-              </div>
-              {errorMessage && (
-                <ErrorMessage
-                  key={errorTimestamp || new Date().getTime()}
-                  message={errorMessage}
-                />
-              )}
-              <p className="text-xs opacity-70">
-                Hesabın yok mu?{" "}
-                <Link href="/sign-up">
-                  <Button variant="link" className="px-0 text-xs">
-                    Kayıt ol
-                  </Button>
-                </Link>
-              </p>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="w-full">
-          <LoginGithub />
-        </CardFooter>
-      </Card>
+            </form>
+          </CardContent>
+          <CardFooter className="w-full">
+            <LoginGithub />
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 }
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -16,6 +16,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { cookies } from "next/headers";
 import ErrorMessage from "@/components/ui/ErrorMessage";
+import Image from "next/image";
 
 export default function page() {
   const cookieStore = cookies();
@@ -30,70 +31,80 @@ export default function page() {
     <div className="flex items-center justify-center md:mt-24 mt-5 px-5">
       <div className="absolute inset-0 bg-grid-pattern -mt-16"></div>
       <div className="absolute inset-0 bg-radial-gradient"></div>
-      <Card className="w-[450px] relative">
-        <CardHeader>
-          <CardTitle>Kayıt Ol</CardTitle>
-          <CardDescription>
-            Kayıt olmak için bilgilerinizi girin.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form action={registerWithCreds}>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Ad Soyad</Label>
-                <Input
-                  id="name"
-                  placeholder="Ad Soyad"
-                  name="name"
-                  type="text"
-                  required
-                />
+      <div className="z-30 flex md:flex-row flex-col gap-20 justify-center items-center bg-zinc-950 p-5 md:p-28 md:px-36 rounded-lg border border-zinc-800">
+        <Image
+          src="/register.png"
+          width={250}
+          height={350}
+          alt="login"
+          unoptimized
+          className="w-[200px] h-[200px] md:w-[450px] md:h-[450px] object-cover rounded-lg"
+        />
+        <Card className="relative bg-transparent border-zinc-900 shadow-lg lg:w-[400px] w-full">
+          <CardHeader>
+            <CardTitle>Kayıt Ol</CardTitle>
+            <CardDescription>
+              Kayıt olmak için bilgilerinizi girin.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form action={registerWithCreds}>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="name">Ad Soyad</Label>
+                  <Input
+                    id="name"
+                    placeholder="Ad Soyad"
+                    name="name"
+                    type="text"
+                    required
+                  />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="name">Email</Label>
+                  <Input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    required
+                  />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="password">Şifre</Label>
+                  <Input
+                    id="password"
+                    placeholder="Şifre"
+                    type="password"
+                    required
+                    name="password"
+                  />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <AuthButton title="Kayıt Ol" />
+                </div>
+                {errorMessage && (
+                  <ErrorMessage
+                    key={errorTimestamp || new Date().getTime()}
+                    message={errorMessage}
+                  />
+                )}
+                <p className="text-xs opacity-70">
+                  Hesabın var mı?{" "}
+                  <Link href="/sign-in">
+                    <Button variant="link" className="px-0 text-xs">
+                      Giriş yap
+                    </Button>
+                  </Link>
+                </p>
               </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Email</Label>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="Email"
-                  required
-                />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="password">Şifre</Label>
-                <Input
-                  id="password"
-                  placeholder="Şifre"
-                  type="password"
-                  required
-                  name="password"
-                />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <AuthButton title="Kayıt Ol" />
-              </div>
-              {errorMessage && (
-                <ErrorMessage
-                  key={errorTimestamp || new Date().getTime()}
-                  message={errorMessage}
-                />
-              )}
-              <p className="text-xs opacity-70">
-                Hesabın var mı?{" "}
-                <Link href="/sign-in">
-                  <Button variant="link" className="px-0 text-xs">
-                    Giriş yap
-                  </Button>
-                </Link>
-              </p>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="w-full">
-          <LoginGithub />
-        </CardFooter>
-      </Card>
+            </form>
+          </CardContent>
+          <CardFooter className="w-full">
+            <LoginGithub />
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 }
