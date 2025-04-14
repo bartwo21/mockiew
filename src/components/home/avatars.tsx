bfd6f23adf673ac14f1d840b9c4c2039dea6a92d
Bartu Çakır <bartuuu_lol@hotmail.com>
2025-04-14 21:03:37 +0300

    feat: page loading and animation for avatars

--- src/components/home/avatars.tsx
+++ src/components/home/avatars.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { CiPlay1 } from "react-icons/ci";
+import { motion } from "framer-motion";
 
 interface CustomAvatarProps {
   src: string;
@@ -15,13 +17,36 @@ export const CustomAvatar: React.FC<CustomAvatarProps> = ({
   iconRotation,
 }) => {
   return (
-    <div className={`absolute ${positionStyles}`}>
-      <CiPlay1 className={`absolute ${iconRotation}`} />
-      <Avatar
-        className={`max-w-20 w-[60px] h-[60px] border-4 border-white ${className}`}
-      >
-        <AvatarImage src={src} />
-      </Avatar>
-    </div>
+    <motion.div
+      className={`absolute ${positionStyles}`}
+      initial={{ scale: 0.95 }}
+      animate={{
+        scale: 1,
+        y: [0, -2, 0],
+        x: [0, 2, 0],
+      }}
+      transition={{
+        duration: 2,
+        ease: "easeInOut",
+        repeat: Infinity,
+        repeatType: "reverse",
+        y: {
+          duration: 3,
+          repeat: Infinity,
+          ease: "easeInOut",
+        },
+      }}
+    >
+      <div className={`absolute ${iconRotation}`}>
+        <CiPlay1 className="text-primary" />
+      </div>
+      <div>
+        <Avatar
+          className={`max-w-20 w-[60px] h-[60px] border-4 border-white transition-all duration-300 hover:border-primary ${className}`}
+        >
+          <AvatarImage src={src} />
+        </Avatar>
+      </div>
+    </motion.div>
   );
 };
